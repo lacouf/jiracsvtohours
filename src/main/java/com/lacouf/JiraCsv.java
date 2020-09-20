391a04cc6ce0238f757df74ef708eb47a4fafa8d
Francois Lacoursiere <lacouf@gmail.com>
2020-09-20 11:46:13 -0400

    Parsing done properly into java class: LogWorkEntry

--- src/main/java/com/lacouf/JiraCsv.java
+++ src/main/java/com/lacouf/JiraCsv.java
@@ -1,7 +1,88 @@
 package com.lacouf;
 
+import com.opencsv.CSVReader;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class JiraCsv {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    static List<LogWorkEntry> logWorkEntries = new ArrayList<>();
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/LLL/yy h:mm a", Locale.US);
+
+    public static void main(String[] args) throws Exception {
+        LocalDateTime date = LocalDateTime.now();
+        LocalDateTime newDate = LocalDateTime.parse(date.format(formatter), formatter);
+        System.out.println(newDate.format(formatter));
+
+        if (args[0].isEmpty()) {
+            System.out.printf("Usage java -jar JiraCSV.jar file.csv");
+            System.exit(0);
+        }
+
+        final Path path = Paths.get(args[0]);
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVReader csvReader = new CSVReader(reader);
+        List<String[]> list = new ArrayList<>();
+        list = csvReader.readAll();
+        reader.close();
+        csvReader.close();
+        Pair<Integer, Integer> logWork = startLogWork(list.get(0));
+        list.stream()
+            .forEach(line -> parseLine(line, logWork));
+        logWorkEntries.stream()
+                      .forEach(System.out::println);
+    }
+
+    private static void parseLine(String[] line, Pair<Integer, Integer> logWork) {
+
+        for (int i = logWork.getLeft(); i < logWork.getLeft() + logWork.getRight(); i++) {
+            final String[] split = rmlf(line[i]).split(";");
+            if (split.length >= 4) {
+                if (!split[0].isEmpty() && !split[1].isEmpty()) {
+                    LogWorkEntry entry = new LogWorkEntry(line[1], line[0], split[2], split[0], split[1],
+                            strToInt(split[3]));
+                    logWorkEntries.add(entry);
+                }
+            }
+        }
+
+    }
+
+    private static int strToInt(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private static Pair<Integer, Integer> startLogWork(String[] line) {
+        int i = 0;
+        int count = 0;
+        int startLogWork = 0;
+        while (!line[i].equals("Log Work")) {
+            i++;
+        }
+        ;
+        startLogWork = i;
+        while (line[i].equals("Log Work")) {
+            count++;
+            i++;
+        }
+        return Pair.of(startLogWork, count);
+    }
+
+    private static String rmlf(String stringToRemoveLf) {
+        return StringUtils.replace(stringToRemoveLf, "\n", "");
     }
 }
---
+++ src/main/java/com/lacouf/LogWorkEntry.java
@@ -0,0 +1,63 @@
+package com.lacouf;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LogWorkEntry {
+    private String taskId;
+    private String userTask;
+    private String userName;
+    private String logWorkDescription;
+    private String logWorkDate;
+    private int logWorkSeconds;
+    private LocalDateTime logWorkDateTime;
+
+    public LogWorkEntry(String taskId, String userTask, String userName, String logWorkDescription, String logWorkDate, int logWorkSeconds) {
+        this.taskId = taskId;
+        this.userTask = userTask;
+        this.userName = userName;
+        this.logWorkDescription = logWorkDescription;
+        this.logWorkDate = logWorkDate;
+        this.logWorkSeconds = logWorkSeconds;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/LLL/yy h:mm a", Locale.US);
+        this.logWorkDateTime = LocalDateTime.parse(logWorkDate, dtf);
+    }
+    @Override
+    public String toString() {
+        return "LogWorkEntry{" +
+                "taskId='" + taskId + '\'' +
+                ", userTask='" + userTask + '\'' +
+                ", userName='" + userName + '\'' +
+                ", logWorkDescription='" + logWorkDescription + '\'' +
+                ", logWorkDate='" + logWorkDateTime + '\'' +
+                ", logWorkSeconds=" + logWorkSeconds +
+                '}';
+    }
+
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public String getUserTask() {
+        return userTask;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getLogWorkDescription() {
+        return logWorkDescription;
+    }
+
+    public String getLogWorkDate() {
+        return logWorkDate;
+    }
+
+    public int getLogWorkSeconds() {
+        return logWorkSeconds;
+    }
+}
