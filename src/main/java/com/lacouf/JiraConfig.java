d833449788be369a0493899869da9899c837afe0
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-10-15 18:39:07 -0400

    Ajout de la recherche par date plutôt que par Sprint

--- src/main/java/com/lacouf/JiraConfig.java
+++ src/main/java/com/lacouf/JiraConfig.java
@@ -6,7 +6,7 @@ public class JiraConfig {
     public static final String API_TOKEN = "ST3MGsubFza4iCxsjxkS3FEC"; //https://id.atlassian.com/manage-profile/security pour générer
     public static final String SITE_URL = "https://420-565-eq1.atlassian.net";
     public static final String PROJECT = "EQ3";
-    public static final String SPRINT_NAME = null;
+    public static final String SPRINT_NAME = "Sprint 3"; //si null, recherche par date seulement
     public static final String START_DATE = "01-01-1900";
     public static final boolean INCLUDE_EMPTY_COMMENT = true;
 
